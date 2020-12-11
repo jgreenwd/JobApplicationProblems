1c46c481e390001110a086e8ca728b68036be2c7
Jeremy Greenwood <34409472+jgreenwd@users.noreply.github.com>
2020-12-11 15:17:15 -0600

    chore: add separate SparkMeter directory

--- SparkMeter/Question1.py
+++ SparkMeter/Question1.py
@@ -1,9 +1,9 @@
 if __name__ == '__main__':
 
     def print_3rds(arr):
-    """ Response to Question 1:
-        In the language of your choice, write code that prints every third element of this list using recursion. [1, 2, 3, 4, 5, 6, 7, 8, 9]. Please create a public Github or Gitlab snippet with your response and submit the link here. *
-    """
+        """ Response to Question 1:
+            In the language of your choice, write code that prints every third element of this list using recursion. [1, 2, 3, 4, 5, 6, 7, 8, 9]. Please create a public Github or Gitlab snippet with your response and submit the link here. *
+        """
         if len(arr) < 3:            # test for invalid input array length
             raise ValueError
         elif len(arr) == 3:         # base case
